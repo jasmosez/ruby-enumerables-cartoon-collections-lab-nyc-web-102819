6b7527023ef6fa3f2664a7f8ff16c35cf1c6b9f2
James Schaffer <jas.schaffer@gmail.com>
2019-09-17 17:03:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index { |item, index|
+    puts "#{index + 1} #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! { |call|
+    "#{call.capitalize}!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.reduce
+  include?()
 end
 
 def find_the_cheese# code an argument here
